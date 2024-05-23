cb8a819b9218904ab6e23b9356e44263e95f1e60
Johnsonn10 <Johnsonn10@wit.edu>
2024-05-23 17:42:24 -0400

    FirstMovementUpdate

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 450f;
+
+    float xrotation = 0f;
+    float yrotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xrotation += mouseY;
+        xrotation = Mathf.Clamp(xrotation, topClamp, bottomClamp);
+
+        yrotation += mouseY;
+
+        transform.localRotation = Quaternion.Euler(xrotation, yrotation, 0f);
+    }
+}
